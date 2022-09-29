9a4d9862c5ab9eeb3355cc2734da606ba80208d8
Dania Haroun <dania.haroun@merwas.net>
2022-09-29 15:50:53 +0300

    done

--- mywork-main-main/src/pages/Home/Footer/components/Section1.jsx
+++ mywork-main-main/src/pages/Home/Footer/components/Section1.jsx
@@ -11,14 +11,14 @@ function Section1() {
       <div>
         <Container >
           <div className=" d-none d-sm-none d-md-block mt-5  ">
-          <div className=" d-xxl-block d-none ">
-            {/* //d-xxl-block d-none */}
+          <div className=" d-xxl-block d-none py-4">
           <div className="service-contain">   
-              <div >           <Row className="lineundersection py-4 ">
+              <div >    
+                       <Row className="lineundersection py-4 ">
 
                 <Col xxl={3} xl={3}>
-                  <div className="service-box">
-                    <div className="service-image">
+                  <div className=" align-items-center d-flex me-0">
+                    <div >
                       <img
                         src="https://themes.pixelstrap.com/fastkart/assets/svg/product.svg"
                         className="blur-up lazyloaded "
@@ -35,15 +35,15 @@ function Section1() {
                 </Col>
                 <Col xxl={3} xl={3}>
                   <div className="service-box">
-                    <div className="service-image ">
+                    <div >
                       <img
                         src="https://themes.pixelstrap.com/fastkart/assets/svg/delivery.svg"
-                        className="blur-up lazyloaded"
+                      className="mx-0"
                         alt=""
                       />
                     </div>
 
-                     <div className="service-detail mx-2"><span className="oneshapeline"></span>
+                     <div className="service-detail mx-2">
 
                       <span>Free Delivery For Order Over $50</span>
                     </div>
@@ -51,15 +51,15 @@ function Section1() {
                 </Col>
                 <Col xxl={3} xl={3}>
                   <div className="service-box">
-                    <div className="service-image">
+                    <div >
                       <img
                         src="https://themes.pixelstrap.com/fastkart/assets/svg/discount.svg"
-                        className="blur-up lazyloaded"
+                      
                         alt=""
                       />
                     </div>
 
-                     <div className="service-detail mx-2"><span className="oneshapeline"></span>
+                     <div className="service-detail mx-2">
 
                       <span>Daily Mega Discounts</span>
                     </div>
@@ -67,15 +67,15 @@ function Section1() {
                 </Col>
                 <Col xxl={3} xl={3}>
                   <div className="service-box justify-content-end d-flex">
-                    <div className="service-image">
+                    <div >
                       <img
                         src="https://themes.pixelstrap.com/fastkart/assets/svg/market.svg"
-                        className="blur-up lazyloaded"
+                      
                         alt=""
                       />
                     </div>
 
-                     <div className="service-detail mx-2"><span className="oneshapeline"></span>
+                     <div className="service-detail mx-2">
 
                       <span>Best Price On The Market</span>
                     </div>
@@ -85,17 +85,17 @@ function Section1() {
             </div>
           </div>
 
-          <div className="  d-lg-block d-xl-block d-xxl-none ">
+          <div className="  d-lg-block d-xl-block d-xxl-none py-4 ">
           
-              <div className="" > 
+              <div  > 
                  <Row >   
                 <Col xl={6} lg={6} md={6}>
-                  <div className="service-box">
-                    <div className="d-flex justify-content-center align-items-center ">
-                    <div className="service-image">
+                  <div >
+                    <div className="d-flex justify-content-start align-items-center ">
+                    <div >
                       <img
                         src="https://themes.pixelstrap.com/fastkart/assets/svg/product.svg"
-                        className="blur-up lazyloaded"
+                      
                         alt=""
                       />
                     </div>
@@ -107,28 +107,30 @@ function Section1() {
                   </div></div>
                 </Col>
                 <Col xl={6} lg={6} md={6}>
-                  <div className="service-box ">  <div className="d-flex justify-content-center align-items-center ">
-                    <div className="service-image">
+                  <div >
+                      <div className="d-flex justify-content-strat align-items-center ">
+                    <div >
                       <img
                         src="https://themes.pixelstrap.com/fastkart/assets/svg/delivery.svg"
-                        className="blur-up lazyloaded"
+                      
                         alt=""
                       />
                     </div>
 
-                     <div className="service-detail mx-2"><span className="oneshapeline"></span>
+                     <div className="service-detail mx-2">
 
                       <span>Free Delivery For Order Over $50</span>
                     </div></div>
                   </div>
                 </Col> </Row>
-                <Row className="lineundersection ">
+                <Row className="lineundersection py-4 ">
                 <Col  xl={6} lg={6} md={6}>
-                  <div className="service-box">  <div className="d-flex justify-content-center align-items-center ">
-                    <div className="service-image">
+                  <div >  
+                  <div className="d-flex justify-content-start align-items-center ">
+                    <div >
                       <img
                         src="https://themes.pixelstrap.com/fastkart/assets/svg/discount.svg"
-                        className="blur-up lazyloaded"
+                      
                         alt=""
                       />
                     </div>
@@ -141,16 +143,17 @@ function Section1() {
                   </div></div>
                 </Col>
                 <Col  xl={6} lg={6} md={6}>
-                  <div className="service-box justify-content-end d-flex">  <div className="d-flex justify-content-center align-items-center ">
-                    <div className="service-image">
+                  <div className="service-box justify-content-start d-flex">  
+                  <div className="d-flex justify-content-center align-items-center ">
+                    <div >
                       <img
                         src="https://themes.pixelstrap.com/fastkart/assets/svg/market.svg"
-                        className="blur-up lazyloaded"
+                      
                         alt=""
                       />
                     </div>
 
-                     <div className="service-detail mx-2"><span className="oneshapeline"></span>
+                     <div className="service-detail mx-2">
 
                       <span>Best Price On The Market</span>
                     </div></div>
